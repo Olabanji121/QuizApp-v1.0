8ff5c4c577a072137b54039462b00303c5706aad
banji <olabanjiabimbola@gmail.com>
2019-09-30 11:56:51 +0100

    code update

--- game.js
+++ game.js
@@ -1,17 +1,4 @@
-// import qs from './question';
-// const questionFile;
-// console.log(qs);
 
-// let questions = [];
-// let noOfQuestion = 5;
-// while (questions.length < noOfQuestion) {
-// 	let que = questionFile[Math.floor(Math.random() * questionFile.length)];
-// 	questions.push(que);
-// }
-
-// let totalQuestion = questions[Math.floor(Math.random() * questions.length)];
-
-// console.log(questions)
 let questionsList = [
 	{
 		question: "Inside which HTML element do we put the JavaScript??",
@@ -125,6 +112,8 @@ let questionsList = [
 		answer: "Michelangelo"
 	}
 ];
+
+
 let questions = [];
 
 while (questions.length < 5) {
@@ -134,71 +123,51 @@ while (questions.length < 5) {
 
 console.log(questions);
 
-// let totalQuestion = questions[Math.floor(Math.random() * questions.length)];
+
 let scoreUI = document.getElementById("scores");
 let pageUI = document.getElementById("pages");
 
 let index = 0;
 let score = 0;
-
-const next = document.querySelector(".next");
-next.addEventListener("click", () => {
-	if (index < questions.length - 1) {
-		index++;
-		loadDetails();
-		checkAnswer.style.display = "block";
-		nextpic.style.display = "none";
-		prev.disabled = false;
-	} else {
-		next.disabled = true;
-		prev.disabled = true;
-		next.textContent = "FInished";
-	}
-
-
-});
-
-const prev = document.querySelector(".prev");
-prev.addEventListener("click", () => {
-	if (index > 0) {
-		next.disabled = false;
-		index--;
-		loadDetails();
-	} else {
-		prev.disabled = true;
-	}
-});
-
 let nextpic = document.querySelector(".right-pic");
 nextpic.style.display = "none";
 
-// console.log(totalQuestion[index].questions);
 startQuiz = () => {
 	loadDetails();
 
 	// checking answer//
 	const checkAnswer = document.getElementById("checkAnswer");
-	// console.log(questions[index].answer);
-	// to disable options
+
 	const disableOpt = Array.from(document.querySelectorAll(".optionText"));
 	console.log(disableOpt);
 
 	checkAnswer.addEventListener("click", e => {
-		// console.log(e.target.textContent)
+	
 		if (e.target.textContent === questions[index].answer) {
 			console.log("right");
-			checkAnswer.style.display = "none";
-			nextpic.style.display = "block";
-			score++;
-			console.log(score);
+			if ( score !== 5 && index === 5) {
+				score++;
+			} else {
+				nextpic.style.display = "block";
+			}
+			
+			// console.log(score);
 			scoreUI.textContent = `score: ${score}`;
+			if (index < questions.length -1) {
+				index++;
+				loadDetails();
+				
+			} 
 		} else {
+			if (index < questions.length - 1) {
+				index++;
+				loadDetails();
+				
+			} 
 			console.log("wrong");
-			// disableOpt.disabled = true;
-			checkAnswer.style.display = "none";
-			nextpic.style.display = "block";
+			
 		}
-		// console.log('right answer' + questions[index].answer);
+		
 	});
 };
 
@@ -217,3 +186,4 @@ loadDetails = () => {
 };
 
 startQuiz();
+
